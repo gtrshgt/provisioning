6a3f9abbd25c90700d6b2b47c80540f62a2e4ac6
gtrshgt <gtrshgt@proton.me>
2024-05-18 16:57:00 -0400

    Update everclear1.sh

--- everclear1.sh
+++ everclear1.sh
@@ -32,8 +32,8 @@ EXTENSIONS=(
 
 CHECKPOINT_MODELS=(
     "https://civitai.com/api/download/models/399640?token=${TOKEN}" # EverclearPNYbyZovya
+    "https://civitai.com/api/download/models/303498?token=${TOKEN}" # softfantasy_realmagic
     #"https://civitai.com/api/download/models/465251?token=${TOKEN}" # PonyRealism
-    #"https://civitai.com/api/download/models/303498?token=${TOKEN}" # softfantasy_realmagic
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
 )
