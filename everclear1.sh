5567e7014d83330d4c2f6a3053935a12d612ae3d
gtrshgt <gtrshgt@proton.me>
2024-07-18 13:38:42 +0200

    Update everclear1.sh

--- everclear1.sh
+++ everclear1.sh
@@ -41,7 +41,8 @@ EXTENSIONS=(
 )
 
 CHECKPOINT_MODELS=(
-    "https://civitai.com/api/download/models/511677?token=${TOKEN}" # Everclear V3
+    "https://civitai.com/api/download/models/534642?token=${TOKEN}" # PonyRealism v2.1 VAE
+    #"https://civitai.com/api/download/models/511677?token=${TOKEN}" # Everclear V3
     #"https://civitai.com/api/download/models/629179?token=${TOKEN}" # boleromix
     #"https://civitai.com/models/476745?modelVersionId=604842?token=${TOKEN}" #  realistic Pony, not working?
     #https://civitai.com/api/download/models/637965 # GoodFit-Pony
@@ -50,12 +51,12 @@ CHECKPOINT_MODELS=(
     #-"https://civitai.com/api/download/models/441236?token=${TOKEN}" # 7th anime XL-Pony A
     #"https://civitai.com/models/413979?modelVersionId=526408?token=${TOKEN}" #Raemora XL 
     #-"https://civitai.com/api/download/models/465251?token=${TOKEN}" # PonyRealism
-    "https://civitai.com/api/download/models/534642?token=${TOKEN}" # PonyRealism v2.1 VAE
     #-"https://civitai.com/models/365041?modelVersionId=515456?token=${TOKEN}" # RealPony real_anime, jap
     ##"https://civitai.com/api/download/models/399640?token=${TOKEN}" # EverclearPNYbyZovya
     ##########"https://civitai.com/api/download/models/324524?token=${TOKEN}" # AutismMixXL_confetti
     #
-    "https://civitai.com/api/download/models/303498?token=${TOKEN}" # softfantasy_realmagic
+    "https://civitai.com/api/download/models/646523?token=${TOKEN}" # epiCRealism XL
+    #"https://civitai.com/api/download/models/303498?token=${TOKEN}" # softfantasy_realmagic
     #"https://civitai.com/api/download/models/228559?token=${TOKEN}" # Omnigen
     ###"https://civitai.com/api/download/models/490639?token=${TOKEN}" # iNiverseMixXL
     #"https://civitai.com/api/download/models/489217?token=${TOKEN}" # epiCRealism XL
