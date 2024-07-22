b520313d0c31b7007d0fcfc46ba5df14354fa27b
gtrshgt <gtrshgt@proton.me>
2024-07-22 23:12:19 +0200

    Update everclear1.sh

--- everclear1.sh
+++ everclear1.sh
@@ -42,8 +42,8 @@ EXTENSIONS=(
 
 CHECKPOINT_MODELS=(
     #"https://civitai.com/api/download/models/534642?token=${TOKEN}" # PonyRealism v2.1 VAE
-    #"https://civitai.com/api/download/models/511677?token=${TOKEN}" # Everclear V3
-    "https://civitai.com/models/443821?modelVersionId=595775&token=${TOKEN}" # CyberRealistic Pony
+    "https://civitai.com/api/download/models/511677?token=${TOKEN}" # Everclear V3
+    ##"https://civitai.com/models/443821?modelVersionId=595775&token=${TOKEN}" # CyberRealistic Pony -> v1-5-pruned-emaonly.safetensors rubish
     #"https://civitai.com/api/download/models/629179?token=${TOKEN}" # boleromix
     #"https://civitai.com/models/476745?modelVersionId=604842?token=${TOKEN}" #  realistic Pony, not working?
     #https://civitai.com/api/download/models/637965 # GoodFit-Pony
