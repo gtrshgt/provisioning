4885a218789877aad8006f37a369041b7e4893ed
gtrshgt <gtrshgt@proton.me>
2024-07-30 23:38:49 +0200

    Update everclear1.sh

--- everclear1.sh
+++ everclear1.sh
@@ -46,7 +46,7 @@ CHECKPOINT_MODELS=(
     #"https://civitai.com/api/download/models/511677?token=${TOKEN}" # Everclear V3
     #"https://civitai.com/api/download/models/595775?token=${TOKEN}" # CyberRealistic Pony , deleted?
     #"https://huggingface.co/cyberdelia/CyberRealisticPony/resolve/main/CyberRealisticPony_V5.safetensors?download=true" # V5 backup
-    "https://civitai.com/api/download/models/253250?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # SDXXXL V3, goodl
+    ###"https://civitai.com/api/download/models/253250?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # SDXXXL V3, goodl, bad bodies
     #"https://civitai.com/api/download/models/629179?token=${TOKEN}" # boleromix
     #"https://civitai.com/models/476745?modelVersionId=604842?token=${TOKEN}" #  realistic Pony, not working?
     #https://civitai.com/api/download/models/637965 # GoodFit-Pony
