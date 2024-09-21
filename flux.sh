db302a0c330c73347523748b2c1c10fa9d46a4d4
gtrshgt <gtrshgt@proton.me>
2024-09-21 15:43:38 -0400

    Update flux.sh

--- flux.sh
+++ flux.sh
@@ -53,6 +53,7 @@ LORA_MODELS=(
     "https://civitai.com/api/download/models/753339?type=Model&format=SafeTensor&token=${TOKEN}" # Phlux V1 
     "https://civitai.com/api/download/models/756149?type=Model&format=SafeTensor&token=${TOKEN}" # Amateur Photography v2.0  'Amateur photography, on flickr in 2007, 2005 blog, 2007 blog'
     "https://civitai.com/api/download/models/736227?type=Model&format=SafeTensor&token=${TOKEN}" # SCG-Anatomy-Flux1.d V2 'scg-anatomy-female'
+    "https://civitai.com/api/download/models/876547?type=Model&format=SafeTensor&token=${TOKEN}" # Cum On Face, V4-beta10
 )
 
 VAE_MODELS=(
