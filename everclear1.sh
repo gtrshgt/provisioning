de0003bd7b554ee26f2bb581bb85fca671aea451
gtrshgt <gtrshgt@proton.me>
2024-10-01 11:36:01 -0400

    Update everclear1.sh

--- everclear1.sh
+++ everclear1.sh
@@ -81,8 +81,7 @@ CHECKPOINT_MODELS=(
     #"https://civitai.com/api/download/models/363977?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # OnlyForNsfw118 v2.0
     #"https://civitai.com/api/download/models/228559?token=${TOKEN}" # Omnigen
     ###"https://civitai.com/api/download/models/490639?token=${TOKEN}" # iNiverseMixXL
-    # SD1.5
-    ##"https://civitai.com/api/download/models/179525?type=Model&format=SafeTensor&size=pruned&fp=fp16" # Rabbit
+    "https://civitai.com/api/download/models/324958?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # Pyro's NSFW SDXL
 )
 
 LORA_MODELS=(
