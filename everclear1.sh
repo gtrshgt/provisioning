8e1bee62b8de93d81dec16ed192796f030d6e24d
gtrshgt <gtrshgt@proton.me>
2025-02-20 20:20:00 -0500

    Update everclear1.sh

--- everclear1.sh
+++ everclear1.sh
@@ -49,7 +49,7 @@ CHECKPOINT_MODELS=(
     #
     #"https://civitai.com/api/download/models/1277531?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # GoR_PONY_v5ilustros_H+VAE!
     #+"https://civitai.com/api/download/models/1087831?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # GoR_PONY_v4photo+VAE!b
-    "https://civitai.com/api/download/models/1410250?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=${TOKEN}" # GoR_v6ILXL+VAE
+    #"https://civitai.com/api/download/models/1410250?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=${TOKEN}" # GoR_v6ILXL+VAE
     #"https://civitai.com/api/download/models/638137?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # GODDESS of Realism Pony_v1.0
     #-"https://civitai.com/api/download/models/578496?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=${TOKEN}" # 2DN-Pony
     #"https://civitai.com/api/download/models/534642?token=${TOKEN}" # PonyRealism v2.1 VAE
