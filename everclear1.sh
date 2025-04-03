32bad381b5f42fae4f3731861da17928fb939ad4
gtrshgt <gtrshgt@proton.me>
2025-04-02 20:38:22 -0400

    Update everclear1.sh

--- everclear1.sh
+++ everclear1.sh
@@ -131,7 +131,7 @@ CHECKPOINT_MODELS=(
     #"https://civitai.com/api/download/models/624942?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=${TOKEN}" # The Araminta Experiment (SDXL+Flux) Cv6 SFW
     ###"https://civitai.com/api/download/models/991916?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=${TOKEN}" # bigASP V2.0  - messy unclear
     "https://civitai.com/api/download/models/1556348?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # PhotoArt v5 
-    "https://civitai.com/api/download/models/1582496?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=${TOKEN}" # Fuchsia XL (Photo) v10
+    #--"https://civitai.com/api/download/models/1582496?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=${TOKEN}" # Fuchsia XL (Photo) v10
     #---"https://civitai.com/api/download/models/1605480?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # EpicJoy Full, 12gb Not loading
 )
 
