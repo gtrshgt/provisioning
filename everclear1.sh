7079964f159d649ad9bfe1f53488d8858b533179
gtrshgt <gtrshgt@proton.me>
2025-05-17 23:59:23 -0400

    Update everclear1.sh

--- everclear1.sh
+++ everclear1.sh
@@ -129,7 +129,7 @@ CHECKPOINT_MODELS=(
     #"https://civitai.com/api/download/models/324958?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # Pyro's NSFW SDXL
     #"https://civitai.com/api/download/models/887555?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # NatViS: Natural Vision V2
     #"https://civitai.com/api/download/models/929239?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # Big Lust v1.5,
-    "https://civitai.com/api/download/models/1081768?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # Big Lust v1.6 Great
+    #++"https://civitai.com/api/download/models/1081768?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # Big Lust v1.6 Great
     #"https://civitai.com/api/download/models/1620724?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=${TOKEN}" # Big Lust v1.7, 1.6 is better
     #"https://civitai.com/api/download/models/1550582?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # LustiMix rebirth
     #++"https://civitai.com/api/download/models/1541389?type=Model&format=SafeTensor&size=full&fp=fp16&token=${TOKEN}" # Bond alpha Great
